26f4ca0f33a289da2b7cd5e5e80288343069ab1f
SAMLab <147920838+GZU-SAMLab@users.noreply.github.com>
2024-11-08 10:43:01 +0800

    Add files via upload

---
+++ visualDet3D/evaluator/kitti/evaluate.py
@@ -0,0 +1,28 @@
+import time
+from .kitti_common import get_label_annos
+from .eval import get_official_eval_result, get_coco_eval_result
+from numba import cuda
+import os
+
+def _read_imageset_file(path):
+    with open(path, 'r') as f:
+        lines = f.readlines()
+    return [int(line) for line in lines]
+
+
+def evaluate(label_path="/home/hins/Desktop/M3D-RPN/data/kitti/training/label_2",
+             result_path="/home/hins/IROS_try/pytorch-retinanet/output/validation/data",
+             label_split_file="val.txt",
+             current_classes=[0],
+             gpu=0):
+    cuda.select_device(gpu)
+    dt_annos = get_label_annos(result_path)
+    val_image_ids=os.listdir(label_path)
+    val_image_ids=sorted(val_image_ids)
+    #val_image_ids=[os.path.splitext(file)[0] for file in val_image_ids]
+    #val_image_ids = _read_imageset_file(label_split_file)
+    gt_annos = get_label_annos(label_path, val_image_ids)
+    result_texts = []
+    for current_class in current_classes:
+        result_texts.append(get_official_eval_result(gt_annos, dt_annos, current_class))
+    return result_texts
